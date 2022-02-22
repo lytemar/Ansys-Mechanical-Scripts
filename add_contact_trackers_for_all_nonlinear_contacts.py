9237ccf91b9f0daef143ec3439520b2a1f8a71b1
lytemar <lytemar@yahoo.com>
2022-02-22 09:18:11 -0600

    first commit add_contact_trackers_for_all_nonlinear_contacts.py

---
+++ add_contact_trackers_for_all_nonlinear_contacts.py
@@ -0,0 +1,52 @@
+"""
+Add contact trackers to Solution Information for all nonlinear contacts
+==================================================================
+"""
+
+model = ExtAPI.DataModel.Project.Model
+analysis = model.Analyses[0]
+sol_info = analysis.Solution.SolutionInformation
+
+# Get all nonliner connections and contacts
+conns = model.Connections
+contacts = conns.GetChildren(DataModelObjectCategory.ContactRegion, True)
+linear_contacts = [ContactType.Bonded, ContactType.NoSeparation]
+contacts = [c for c in contacts if c.ContactType not in linear_contacts]
+supp_conts = [c for c in contacts if c.Suppressed]
+
+# Create contact trackers
+with Transaction():         # Suppress GUI update until complete to speed the process
+    for c in contacts:
+        _ = sol_info.AddNumberContacting()
+        _.ContactRegion = c
+        _.RenameBasedOnDefinition()
+    
+    for c in contacts:
+        _ = sol_info.AddContactPressure()
+        _.ContactRegion = c
+        _.RenameBasedOnDefinition()
+        
+    for c in contacts:
+        _ = sol_info.AddPenetration()
+        _.ContactRegion = c
+        _.RenameBasedOnDefinition()
+    
+    for c in contacts:
+        _ = sol_info.AddContactMaximumNormalStiffness()
+        _.ContactRegion = c
+        _.RenameBasedOnDefinition()
+        
+    for c in contacts:
+        _ = sol_info.AddStabilizationEnergy()
+        _.ContactRegion = c
+        _.RenameBasedOnDefinition()
+        
+    for c in contacts:
+        _ = sol_info.AddContactPairForceConvergenceNorm()
+        _.ContactRegion = c
+        _.RenameBasedOnDefinition()
+    
+    for c in contacts:
+        _ = sol_info.AddGap()
+        _.ContactRegion = c
+        _.RenameBasedOnDefinition()
